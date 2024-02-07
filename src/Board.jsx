b7bd6eccfacc28ec4d77fa5426116f255307bdf2
mmkamal021 <114946934+mmkamal021@users.noreply.github.com>
2024-02-07 21:43:00 +0600

    Tic-tac-toe is ready without history component

--- src/Board.jsx
+++ src/Board.jsx
@@ -4,8 +4,18 @@ import Square from "./Square";
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = "Next Player " + (xIsNext ? "X" : "O");
+  }
+
   function handClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       console.log("clicked!");
       return;
     }
@@ -23,6 +33,7 @@ export default function Board() {
   console.log(squares);
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Square value={squares[0]} onSqureClick={() => handClick(0)} />
         <Square value={squares[1]} onSqureClick={() => handClick(1)} />
@@ -40,4 +51,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
